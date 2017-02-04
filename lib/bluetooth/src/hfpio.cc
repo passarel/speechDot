ac68a5f70152f1c30bf9acf1236139ccb3c2c7ce
Your Name <you@example.com>
2017-02-04 06:00:04 +0000

    converted some hfpio code to c for optimization

--- lib/bluetooth/src/hfpio.cc
+++ lib/bluetooth/src/hfpio.cc
@@ -2,6 +2,81 @@
 
 namespace hfpio {
 
+	const int MTU_SIZE = 48;
+
+	uint8_t parser_buf[60];
+	int parser_buf_len = 0;
+
+	int copy(uint8_t byte) {
+		parser_buf[parser_buf_len] = byte;
+		parser_buf_len++;
+		return parser_buf_len;
+	}
+
+	int msbc_copy_machine(uint8_t byte) {
+		assert(parser_buf_len < 60);
+		switch (parser_buf_len) {
+			case 0:
+				if (byte == 0x01)
+					return copy(byte);
+				return 0;
+			case 1:
+				if (byte == 0x08 || byte == 0x38 || byte == 0xC8 || byte == 0xF8)
+					return copy(byte);
+				break;
+			case 2:
+				if (byte == 0xAD)
+					return copy(byte);
+				break;
+			case 3:
+				if (byte == 0x00)
+					return copy(byte);
+				break;
+			case 4:
+				if (byte == 0x00)
+					return copy(byte);
+				break;
+			default:
+				return copy(byte);
+		}
+		parser_buf_len = 0;
+		return 0;
+	}
+
+	int audio_decode(sbc_t *sbc, uint8_t *data, int data_len, uint8_t *out, int out_len) {
+		assert(data_len == MTU_SIZE);
+		int total_written = 0;
+		for (int i = 0; i < data_len; i++) {
+			if (msbc_copy_machine(data[i]) == 60) {
+				int written = 0;
+				int decoded = 0;
+				char *to_decode = (char *) parser_buf + 2;
+				decoded = sbc_decode(sbc, to_decode, 57, out, out_len, (size_t *) &written);
+				if (decoded > 0) {
+					total_written += written;
+				} else {
+					printf("hfpio: Error while decoding, decoded -> %d \n", decoded);
+				}
+				parser_buf_len = 0;
+			}
+		}
+		return total_written;
+	}
+
+	NAN_METHOD(ResetParserBuf) {
+		parser_buf_len = 0;
+	}
+
+	NAN_METHOD(AudioDecode) {
+		sbc_t *sbc = reinterpret_cast<sbc_t *>(UnwrapPointer(info[0]));
+		unsigned char *input_buf = (unsigned char *) node::Buffer::Data(info[1].As<Object>());
+		int input_buf_len = info[2]->Int32Value();
+		unsigned char *output_buf = (unsigned char *) node::Buffer::Data(info[3].As<Object>());
+		int output_buf_len = info[4]->Int32Value();
+		int written = audio_decode(sbc, input_buf, input_buf_len, output_buf, output_buf_len);
+		info.GetReturnValue().Set(written);
+	}
+
 	void write_async(uv_work_t *req) {
 		socket_io_args_t *socket_io_args = static_cast<socket_io_args_t *>(req->data);
 		socket_io_args->return_val = write(socket_io_args->fd, socket_io_args->buf, socket_io_args->buf_len);
@@ -138,6 +213,9 @@ namespace hfpio {
 
 		Nan::SetMethod(exports, "poll", PollAsync);
 		Nan::SetMethod(exports, "closeFd", CloseFd);
+
+		Nan::SetMethod(exports, "audioDecode", AudioDecode);
+		Nan::SetMethod(exports, "resetParserBuf", ResetParserBuf);
 	}
 
 	NODE_MODULE(hfpio, init);
